b2cdae7594941d94594cfc61994362bb71b03c7a
torosyanysel <torosyanysel@gmail.com>
2025-03-13 11:20:58 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,8 @@
+// Randomly generated TypeScript code
+
+function getRandomNumber(max: number): number {
+  return Math.floor(Math.random() * max);
+}
+
+const numbers = [0, 1, 2, 3, 4, 5];
+numbers[getRandomNumber(numbers.length)];
