153cb012de050cf3a7a0c1e66fe1f40e8e5b8fee
torosyanysel <torosyanysel@gmail.com>
2025-03-14 06:38:36 +0000

    Add 7.ts

---
+++ src/7.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
