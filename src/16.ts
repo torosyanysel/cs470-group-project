0fb88c070a670dca14da32a4c7ca05d22b6568ee
torosyanysel <torosyanysel@gmail.com>
2025-03-23 08:50:09 +0000

    Add 16.ts

---
+++ src/16.ts
@@ -0,0 +1,9 @@
+function printNumbersUpTo(limit: number): void {
+  let i = 1;
+  while (i <= limit) {
+    console.log(i);
+    i++;
+  }
+}
+
+printNumbersUpTo(10); // Output: 1 2 3 4 5 6 7 8 9 10
