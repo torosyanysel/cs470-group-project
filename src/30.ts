032df989b4366ad449aab1e8a6fcbcd38eb5f80b
torosyanysel <torosyanysel@gmail.com>
2025-04-25 00:48:54 +0000

    Add 30.ts

---
+++ src/30.ts
@@ -0,0 +1,13 @@
+class MyClass {
+    constructor() {
+        console.log("MyClass created");
+    }
+
+    method1(): void {
+        console.log("Method 1 called");
+    }
+}
+
+const myInstance = new MyClass();
+
+myInstance.method1();
